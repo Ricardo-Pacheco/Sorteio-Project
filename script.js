054299ae6b76cdfad103b2202889de9b11bb3e88
Ricardo-Pacheco <ricksouzapv@gmail.com>
2023-12-28 15:55:03 -0300

    alteração no arquivo script.js

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ function sortear() {
     const max = Math.floor(document.querySelector(".input-right").value)
     const resultInput = document.querySelector(".result-input")
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    if (min >= max) {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo")
+    } else {
+        
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    resultInput.value = result
+        resultInput.value = result 
+    }
+
+
 }
 
 button.addEventListener("click", sortear)
